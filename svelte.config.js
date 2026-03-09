a1cc1dc2b0c3bff8b0bf678c53f14bc987367718
BL-CZY <jiguangllsw@gmail.com>
2026-03-09 21:40:59 +0000

    path

--- svelte.config.js
+++ svelte.config.js
@@ -5,18 +5,18 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: dev ? '' : '/CloudSIG.github.io'
-		}
-	}
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: dev ? '' : '/'
+        }
+    }
 };
 
 
